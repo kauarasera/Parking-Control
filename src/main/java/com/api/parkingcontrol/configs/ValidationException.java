88b5f46aebbb4cdf125fb7b3cddf1d2bbc20beba
Kauã Rasera <kauarasera@gmail.com>
2023-11-10 13:24:26 -0300

    Passando as verificações que estão na Controller para Service
    Conforme boas praticas toda regra de negocio deve ficar na Service

---
+++ src/main/java/com/api/parkingcontrol/configs/ValidationException.java
@@ -0,0 +1,8 @@
+package com.api.parkingcontrol.configs;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String s) {
+        super(s);
+    }
+}
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,15 +1,25 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.configs.ValidationException;
+import com.api.parkingcontrol.controllers.ParkingSpotController;
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class ParkingSpotService {
@@ -23,6 +33,20 @@ public class ParkingSpotService {
 
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+
+        //Depois poderá ser essas responsabilidades isolado essas reponsabilidades em um custom validate
+        if (existsByLicensePlateCar(parkingSpotModel.getLicensePlateCar())) {
+            throw new ValidationException("Conflict: License Plate Car is already in use!");
+        }
+        if (existsByParkingSpotNumber(parkingSpotModel.getParkingSpotNumber())) {
+            throw new ValidationException("Conflict: Parking Spot is already in use!");
+        }
+
+        if (existsByRoomAndFloor(parkingSpotModel.getRoom(), parkingSpotModel.getFloor())) {
+            throw new ValidationException("Conflict: Parking Spot already registered for this room/floor!");
+        }
+
+        parkingSpotModel.setReservationDate(LocalDateTime.now(ZoneId.of("UTC")));
         return parkingSpotRepository.save(parkingSpotModel);
     }
 
@@ -40,11 +64,22 @@ public class ParkingSpotService {
     }
 
     public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        Page<ParkingSpotModel> parkingSpotModelList =  parkingSpotRepository.findAll(pageable);
+        if (!parkingSpotModelList.isEmpty()) {
+            for (ParkingSpotModel spot : parkingSpotModelList) {
+                UUID id = spot.getId();
+                spot.add(linkTo(methodOn(ParkingSpotController.class).getOneParkingSpot(id)).withSelfRel());
+            }
+        }
         return parkingSpotRepository.findAll(pageable);
     }
 
     public Optional<ParkingSpotModel> findById(UUID id) {
-        return parkingSpotRepository.findById(id);
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotRepository.findById(id);
+        if (parkingSpotModelOptional.isEmpty()) {
+            throw new ValidationException("Parking Spot not found");
+        }
+        return parkingSpotModelOptional;
     }
 
     @Transactional //foi anotado aqui pois é um metodo destrutivo caso der algo errado eu tenho um rollback
